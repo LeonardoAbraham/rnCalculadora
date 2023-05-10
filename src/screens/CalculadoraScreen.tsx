bf625a977aba6a3b928d972d566b3f7fead8aeef
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-05-10 11:23:05 -0600

    Diseño inicial y pantalla de Calculadora

---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import { styles } from '../theme/appTheme';
+
+export const CalculadoraScreen = () => {
+  return (
+        <View>
+            <Text style={styles.texto}>CalculadoraScreen</Text>
+        </View>
+    )
+}
