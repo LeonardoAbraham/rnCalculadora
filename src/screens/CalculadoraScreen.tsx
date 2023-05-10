35ffcb592366071d8db5aea67e3e7d68ae10eab0
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-05-10 12:39:54 -0600

    Textos y mi primer boton

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native';
 import { styles } from '../theme/appTheme';
 
 export const CalculadoraScreen = () => {
   return (
-        <View>
-            <Text style={styles.texto}>CalculadoraScreen</Text>
+        <View style={styles.calculadoraContainer}>
+            <Text style={styles.resultadoPequeno} >1,500.00</Text>
+            <Text style={styles.resultado}>1,500.00</Text>
+
+
+            <View>
+                <View style={styles.boton}>
+                    <Text style={styles.botonTexto}>1</Text>
+                </View>
+            </View>
         </View>
     )
 }
